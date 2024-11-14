7293af3e272f131e143fafebc3b70290007b0796
ilyass <ilyassezzaouya4@gmail.com>
2024-11-14 14:18:38 +0100

    api setuping

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,28 +1,41 @@
 import React from 'react'
 import { useEffect,useState } from 'react'
 import{copy,linkIcon,loader,tick} from '../assets'
+import { useLazyGetSummaryQuery } from '../services/article'
 
 
 
 const Demo = () => {
 
-  const [Article,setArticle]=useState({
+  const [article,setArticle]=useState({
     url:"",
     summary:"",
   })
-  async function handlesubmit(e){
-    alert('submit')
-    e.preventDefault()
-  }
+  const [getSummary,{error,isFetching}]=useLazyGetSummaryQuery()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    // Call getSummary with the correct parameter structure
+    const { data } = await getSummary({ articleUrl: article.url });
+    
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+  
+
   return (
     <section className='mt-16 w-full max-w-xl'>
 {/* search bar */}
 <div className='flex flex-col w-full gap-2'>
-  <form className='relative flex justify-center items-center' onSubmit={()=>{}}>
+  <form className='relative flex justify-center items-center' onSubmit={handleSubmit}>
     <img src={linkIcon} alt="linkicaon" className=' absolute left-0 my-2 ml-3 w-5' />
   
-  <input className='url_input peer' type="url"  placeholder='enter a URL' value={Article.url} onChange={(e)=>{setArticle({...Article,url:e.target.value})}} required />
-  <button type='submit' className='submit_btn px-8 peer-focus:border-gray-700 peer-focus:text-gray-700' > Submit</button>
+  <input className='url_input peer' type="url"  placeholder='enter a URL' value={article.url} onChange={(e)=>{setArticle({...article,url:e.target.value})}} required />
+  <button type='submit' className='submit_btn px-8 peer-focus:border-gray-700 peer-focus:text-gray-700'onClick={handleSubmit} > Submit</button>
   </form>
 
   {/* browser url history */}
--- src/services/store.jsx
+++ src/services/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { articleApi } from "./article";
-export const store = configureStore({
+export  const store = configureStore({
     reducer: {
         [articleApi.reducerPath]: articleApi.reducer,
     },
